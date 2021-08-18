3ac7ab80ee10698081e004f1c73c3b84e316fa1c
José Antonio Puértolas Montañés <63794670+jose-nferx@users.noreply.github.com>
2021-08-18 03:49:38 +0200

    fonts

--- Casks/fonts.rb
+++ Casks/fonts.rb
@@ -1,5 +1,5 @@
 cask "fonts" do
-  sha256 "c79b2b9ae8da431134b214f96c6a3b2c2598a3cc5a8db7764a3c789c683dc767"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   url "https://github.com/j5pu/fonts/archive/refs/tags/v0.1.0.tar.gz"
   name "Tools Fonts"
